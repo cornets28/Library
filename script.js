6c21ffd135d3449eec2e3c46d0c45ba543534e96
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-14 22:29:32 +0530

    TODO: Style the UI

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 let myLibrary = [
 	new Book(
-		'Sample book 1',
-		'sample description',
+		0,
+		'Sample Book',
+		'Sample author',
+		100,
+		true,
 		'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
 	),
 	new Book(
-		'Sample book 2',
-		'sample description',
+		1,
+		'Sample Book 1',
+		'Sample author 1',
+		100,
+		true,
 		'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
 	),
 	new Book(
-		'Sample book 3',
-		'sample description',
+		2,
+		'Sample Book 2',
+		'Sample author 2',
+		100,
+		true,
 		'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
 	),
 	new Book(
-		'Sample book 4',
-		'sample description',
-		'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-	),
-	new Book(
-		'Sample book 5',
-		'sample description',
+		3,
+		'Sample Book 3',
+		'Sample author 3',
+		100,
+		true,
 		'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
 	)
 ];
@@ -30,34 +37,54 @@ render();
 
 function render() {
 	let library = document.querySelector('.library');
-	myLibrary.forEach((book, index) => {
-		let bookID = myLibrary.length;
+	library.innerHTML = '';
+	myLibrary.forEach(book => {
 		let bookHtml = `
-    <div id=book-${bookID}>
-      <p>${book.name}</p>
-      <p>${book.description}</p>
-      <img src=${book.imageUrl} alt=${book.name}>
+    <div id=book-${book.id}>
+      <p>${book.title}</p>
+      <p>${book.author}</p>
+      <p>${book.pages}</p>
+      <p>Is Read: ${book.isRead}</p>
+      <img src=${book.image} alt=${book.name}>
+      <button>X</button>
     </div>`;
 		library.insertAdjacentHTML('beforeend', bookHtml);
 	});
 }
 
-function Book(name, description, imageUrl) {
-	this.name = name;
-	this.description = description;
-	this.imageUrl = imageUrl;
+function Book(id, title, author, numPages, isRead, image) {
+	this.id = id;
+	this.title = title;
+	this.author = author;
+	this.numPages = numPages;
+	this.isRead = isRead;
+	this.image = image;
 }
 
-function addBookToLibrary() {
-	const name = document.querySelector('#book-name').value;
-	const description = document.querySelector('#book-description').value;
+function addBookToLibrary(e) {
+	e.preventDefault();
+	const author = document.querySelector('#book-author').value;
+	const title = document.querySelector('#book-title').value;
+	const numPages = document.querySelector('#book-numPages').value;
+	const isRead = document.querySelector('#book-isRead').checked;
 	const image = document.querySelector('#book-image').value;
 
-	const book = new Book(name, description, image);
+	// Create id from last item's id
+	let id;
+	if (myLibrary.length > 0) {
+		id = myLibrary[myLibrary.length - 1].id + 1;
+	} else {
+		id = 0;
+	}
 
+	const book = new Book(id, title, author, numPages, isRead, image);
 	myLibrary.push(book);
 
 	render();
 }
 
 document.querySelector('#add-book').addEventListener('click', addBookToLibrary);
+
+document.querySelector('.library').addEventListener('click', event => {
+	let deleteId = event.target.parentNode.id.split('-')[1];
+});
