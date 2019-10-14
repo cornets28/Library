19c4fdbd50f708cbec815c4199238a45d97e86a0
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-14 21:50:56 +0530

    Display seed books data

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-let myLibrary = [];
+let myLibrary = [
+	new Book(
+		'Sample book 1',
+		'sample description',
+		'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+	),
+	new Book(
+		'Sample book 2',
+		'sample description',
+		'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+	),
+	new Book(
+		'Sample book 3',
+		'sample description',
+		'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+	),
+	new Book(
+		'Sample book 4',
+		'sample description',
+		'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+	),
+	new Book(
+		'Sample book 5',
+		'sample description',
+		'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+	)
+];
+
+render();
+
+function render() {
+	let library = document.querySelector('.library');
+	myLibrary.forEach((book, index) => {
+		let bookID = myLibrary.length;
+		let bookHtml = `
+    <div id=book-${bookID}>
+      <p>${book.name}</p>
+      <p>${book.description}</p>
+      <img src=${book.imageUrl} alt=${book.name}>
+    </div>`;
+		library.insertAdjacentHTML('beforeend', bookHtml);
+	});
+}
 
 function Book(name, description, imageUrl) {
 	this.name = name;
@@ -14,6 +56,8 @@ function addBookToLibrary() {
 	const book = new Book(name, description, image);
 
 	myLibrary.push(book);
+
+	render();
 }
 
 document.querySelector('#add-book').addEventListener('click', addBookToLibrary);
